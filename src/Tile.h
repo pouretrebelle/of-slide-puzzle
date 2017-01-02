fc1580c8dbcb2a8abe241b1cf2ccb57c096e6957
Charlotte Dann <ask@pouretrebelle.com>
2017-01-02 10:30:34 +0000

    :art: Arrange Tile.h variables by scene

--- src/Tile.h
+++ src/Tile.h
@@ -5,48 +5,57 @@ class Tile {
 
 public:
 
-  void move(ofVec2f dir, bool easing);
-
-  void setupS2();
-
+  Tile(int _x, int _y, float _w, float _h, float _gutter);
   void update(int frameCounter, float secondsElapsed);
-  void updateS0(int frameCounter);
-  void updateS1(int frameCounter);
-  void updateS2(int frameCounter);
-
   void draw();
-  void drawS0();
-  void drawS1();
-  void drawS2();
+
+  int curFrame;
+  int scene;
+  float* transitions;
+
+
+  // Scene 0
+  //===================================
 
   ofImage image;
   bool moving = false;
   bool updateImage;
   int initialX, initialY;
-  int curFrame;
-  int scene;
-  float* transitions;
-  float startX, startY, endX, endY;
-  float x, y;
   float w, h, gutter;
-
-  // Scene 0
+  float x, y;
+  float startX, startY, endX, endY;
   int tileMoveAnimFrames;
+  void move(ofVec2f dir, bool easing);
+  void updateS0(int frameCounter);
+  void drawS0();
+
 
   // Scene 1
+  //===================================
+
   int tileSizeAnimFrames;
   float dotSize;
+  void drawS1();
+  void updateS1(int frameCounter);
+
 
   // Scene 2
+  //===================================
+
   ofVec2f pos;
   ofVec2f vel;
   ofColor color;
   float speed;
+  void setupS2();
+  void updateS2(int frameCounter);
+  void drawS2();
+
+
+  // Helper Functions
+  //===================================
 
   float easeOutCubic(float t, float b, float c, float d);
   float easeInCubic(float t, float b, float c, float d);
-
-  Tile(int _x, int _y, float _w, float _h, float _gutter);
 
 private:
 
